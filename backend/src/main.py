c6a7288b8e178dd0c984b4752d5897adb16fcb95
NeuroTalos <neurotalos.26@yahoo.com>
2025-02-02 12:38:34 +0800

    Added dockerfiles, docker-compose file and requirements

--- backend/src/main.py
+++ backend/src/main.py
@@ -12,6 +12,10 @@ app.include_router(router_crypto)
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://localhost:4173",
+    "http://127.0.0.1:4173",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 app.add_middleware(
